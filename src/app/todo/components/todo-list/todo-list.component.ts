5ff9eff57b57c203848aef5109afd799e2f3550d
Harry <harry74323@gmail.com>
2020-08-02 16:05:49 +1000

    async data into list

--- src/app/todo/components/todo-list/todo-list.component.ts
+++ src/app/todo/components/todo-list/todo-list.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {ToDo} from '../../models/toDo';
+import {Observable} from 'rxjs';
+import {ToDoEntityService} from '../../services/todo-entity.service';
 
 @Component({
   selector: 'todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
-export class TodoListComponent {
+export class TodoListComponent implements OnInit{
+  todos$: Observable<ToDo[]>;
 
+  constructor(
+    private todoService: ToDoEntityService) {
+  }
+
+  ngOnInit() {
+    this.todos$ = this.todoService.entities$
+  }
 }
